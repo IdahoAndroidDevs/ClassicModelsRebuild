a4e7be668671a05a4df09fc661382097cbb0e97e
Joe Rowley (jrowley) <joseph.rowley@mlb.com>
2017-02-19 23:27:28 -0700

    Setting up Custom Application

---
+++ app/src/main/java/meetup/idahoandroid/classicmodelsrebuild/ClassicModelsApplication.java
@@ -0,0 +1,23 @@
+package meetup.idahoandroid.classicmodelsrebuild;
+
+import android.app.Application;
+
+
+public class ClassicModelsApplication extends Application {
+
+    private static ClassicModelsApplication application;
+
+    public static ClassicModelsApplication getInstance() {
+        return application;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setUpApplicationComponent();
+    }
+
+    private void setUpApplicationComponent() {
+        // TODO: 2/19/17  
+    }
+}
